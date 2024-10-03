b0a7923b1437fb3c416c22c30515dc9b755fd115
Kroos <hamroliyevahmadjon1909@gmail.com>
2024-10-03 15:09:57 +0500

    BROKERS: Create OpenAIBroker

---
+++ HSpeach.Portal/Brokers/OpenAIs/IOpenAIBroker.cs
@@ -0,0 +1,10 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE ALL GOOD HEART Developers
+// ---------------------------------------------------------------
+
+namespace HSpeach.Portal.Brokers.OpenAIs
+{
+    public partial interface IOpenAIBroker
+    { }
+}
